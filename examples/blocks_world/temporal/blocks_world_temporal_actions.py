c4a6d05429a2ed6227dd6bc58a8f0393eead45c7
pzaidins2 <paulzaidins2@gmail.com>
2026-03-19 23:11:57 -0400

    more work on helper functions for accessing and manipulating temporal state

--- examples/blocks_world/temporal/blocks_world_temporal_actions.py
+++ examples/blocks_world/temporal/blocks_world_temporal_actions.py
@@ -17,9 +17,9 @@ note: we handle time points by integer label rather than temporal value
 where appropriate we store a list and a last valid index (everything past this index is garbage)
 state description
 state: ipyhop state object
-    object_var: list of tuples (time_point, predicate_name, *predicate_args) where
-        predicate_name( *predicate_args ) is true of the state at time_point
-    temporal_var: list of tuples (time_point_0, comp_op, int_eval) where time_point_0 is a time point
+    object_var: list of tuples (time_point, predicate_name, *predicate_arg, value) where
+        predicate_name( predicate_arg ) equals value at time_point 
+    temporal_con: list of tuples (time_point_0, comp_op, int_eval) where time_point_0 is a time point
         comp_op is a comparison operator and int_eval is an expression that evaluates to an int (usually a time point
         or fixed integer)
     t_now: latest time point for which all incoming effects have been resolved
@@ -27,13 +27,12 @@ state: ipyhop state object
         time point is earlier (temporal relation) t_ordered[i] <= t_ordered[j] for all i<=j
     t_unordered: unordered list of time points yet to be placed into total ordering
     t_all: unordered list of all time points (t_0 has the label found at t_all[0])
-    persistences: list of tuples (t_start, t_end, desired_bool, predicate_name, *predicate_args)
-        where for all time points between t_start and t_end (predicate_name, *predicate_args) holds true
-        if desired_bool is true else it must hold false
+    persistences: list of tuples (t_start, t_end, desired_bool, predicate_name, predicate_arg)
+        where for all time points between t_start and t_end predicate_name( predicate_args) must not contradict value
     domain_objects: fixed typing of all domain objects
         blocks: list of all objects of type Blocks
         table: singular table object (type Table)
-        surfaces: list of all objects of type Surface
+        surfaces: list of all objects of type Surface 
 '''
 
 # helper functions
@@ -59,6 +58,107 @@ def safe_list_update( lst: List, update_element_lst: List,  update_start_index: 
         lst.extend(update_element_lst[i:])
     return last_valid_index
 
+# returns generator that yields potentially relevant variable assignments
+# given a list of tuples, the last valid index, and a predicate and list of predicate values return
+# the first match going in reverse order of indices
+# given (t_0, on, A, B), (t_0, on, B, C), (t_1, on, A, T), (t_1, clear, A)
+# the input (on,) would give (t_1, on, A, T) and input (on,B) would
+# give (t_0,on,B,C)
+# if no pattern match returns empty generator else returns generator for matching pattern
+def object_variable_lookup_generator( object_var_lst: List[Tuple], last_valid_index: int, pred_pat: Tuple, t_ordered: List[int],
+                                      t_now: int):
+    # cycle through list in reverse
+    for i in reversed(range(last_valid_index+1)):
+        current_tuple = object_var_lst[i]
+        for j in range(len(pred_pat)):
+            # break out of inner loop on mismatch
+            if current_tuple[j+1] != pred_pat[j]:
+                break
+            # match found if every arg in pred_pat matched
+            if j == len(pred_pat)-1:
+                # make sure the time point is before t_now in the ordering
+                t_then = current_tuple[0]
+                now_index = t_ordered.index(t_now)
+                then_index = t_ordered.index(t_then)
+                if now_index >= then_index:
+                    match = current_tuple
+                    yield match
+
+# returns generator which yields potentially relevant temporal constraints
+# the following are considered relevant
+def temporal_constraint_lookup_generator( temporal_con_lst: List[Tuple], last_valid_index: int, temporal_pat: Tuple,
+                                          t_ordered: List[int], t_now: int ):
+    # # cycle through list in reverse starting at last valid index
+    # for temporal_con in reversed(temporal_con_lst[:last_valid_index+1]):
+    #     t_con_0, t_op, t_con_1 = temporal_con
+    pass
+
+
+
+# check for evaluated predicate contradicting any member of the persistent conditions
+# a contradiction requires the current time point to be within the bound of a persistence condition starting and
+# ending times with the predicate and predicate arguements matching and the values being different
+# only persistence conditions at indices smaller than last valid index count
+def persistence_check( pers_con_lst: List[Tuple], pers_con_last_val_idx: int,
+                       eval_pred: Tuple, t_ordered: List[int], t_now: int ):
+    # iterate in reverse over persistences within last valid index
+    for pers_con in reversed(pers_con_lst[:pers_con_last_val_idx+1]):
+        # check if t_now is within persistence condition bound
+        t_start = pers_con[ 0 ]
+        t_end = pers_con[ 1 ]
+        t_now_idx = t_ordered.index(t_now)
+        t_start_idx = t_ordered.index(t_start)
+        t_end_idx = t_ordered.index( t_end )
+        if t_now_idx >= t_start_idx and t_now_idx <= t_end_idx:
+            # check if predicate name and args match
+            if eval_pred[1:-1] == pers_con[2:-1]:
+                # check if value contradicts
+                if eval_pred[-1] != pers_con[-1]:
+                    return False
+    return True
+
+
+# return true if no contradiction in state would be created by adding new tuple
+# object variable 
+def contradiction_check( lst_type: str, lst: List[Tuple], eval_pred: Tuple, t_ordered: List[int], t_now: int ):
+    if lst_type == "object":
+        return object_contradiction_check()
+    elif lst_type == "temporal":
+        return temporal_contradiction_check()
+    else:
+        raise ValueError(lst_type + "is not a supported value for the type of list")
+
+# if an assignment at the time of the predicate exists already, returns False if the value is different else
+# returns True
+def object_contradiction_check(object_var_lst: List[Tuple], last_valid_index: int, pred_pat: Tuple,
+                               t_ordered: List[int], t_now: int):
+    # find any assignment at t_now for the predicate
+    rel_obj_gen = object_variable_lookup_generator( object_var_lst, last_valid_index, pred_pat[:-1], t_ordered, t_now)
+    for rel_obj_var in rel_obj_gen:
+        # check if value is different
+        if rel_obj_var[-1] != pred_pat[-1]:
+            return False
+    return True
+
+def temporal_contradiction_check():
+    pass
+
+# return true if tuple can be added without contradiction or persistence condition violation
+def can_add_assertion():
+    if contradiction_check() and persistence_check():
+        return True
+# given list of assertions check to see if they can all be added without issue
+def can_add_all_assertions():
+
+    pass
+
+
+# get all time points that are plausible candidates for next time point to add to ordering
+# find all timepoints that have no timepoint not in t_ordered that must precede them
+def next_timepoint_generator():
+    pass
+
+# ADD universal low priority methods for advancing time and choosing next timepoint
 
 #
 # actions = Actions()
