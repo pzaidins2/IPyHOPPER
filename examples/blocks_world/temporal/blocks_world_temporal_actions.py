071d1bbb44d74718c950ce66231279ecfa402bc7
pzaidins2 <paulzaidins2@gmail.com>
2026-03-21 22:56:17 -0400

    Update blocks_world_temporal_actions.py

--- examples/blocks_world/temporal/blocks_world_temporal_actions.py
+++ examples/blocks_world/temporal/blocks_world_temporal_actions.py
@@ -108,22 +108,125 @@ class TemporalNetwork:
     # t_max: the latest time a time point may instantiate to
     def __init__( self, t_min: int, t_max: int ):
         # full temporal network as directed graph allowing parallel edges
-        full = MultiDiGraph()
+        self.full = MultiDiGraph()
         # minimal representaion of above as simple directed graph
-        minimal = DiGraph()
+        self.minimal = DiGraph()
         self.t_min = t_min
         self.t_max = t_max
         # virtual time points for fixed integer positions -1 is t_min and -2 is t_max
         # negative numbers are used here to avoid name clashes with planner
         virtual_tps = [ -1, -2 ]
-        full.add_nodes_from( virtual_tps )
-        minimal.add_nodes_from( virtual_tps )
+        self.full.add_nodes_from( virtual_tps )
+        self.minimal.add_nodes_from( virtual_tps )
 
     # update full and minimal STN with edge in TIPyHOPPER format: (t_0, op, t_1, int)
-    # returns tuple (success_flag, full_add_lst, minimal_add_list, minimal_remove_list)
-    def _add_constraint( self, t_con: Tuple[int,str,int,int] ) -> (
-            Tuple[bool,List[NetEdgeInput],List[NetEdgeInput],List[NetEdgeInput]]):
+    # returns tuple (success_flag, node_add_lst, full_add_lst, minimal_add_lst, minimal_remove_lst)
+    def _add_constraint( self, new_edge: NetEdgeInput ):
+        # make local refs
+        full: MultiDiGraph = self.full
+        minimal: DiGraph= self.minimal
+        get_bounding_edges = self.get_bounding_edges
+        # initialize lists
+        node_add_lst: List[int] = []
+        full_edge_add_lst: List[NetEdgeInput] = []
+        minimal_edge_add_lst: List[NetEdgeInput] = []
+        minimal_edge_remove_lst: List[NetEdgeInput] = []
+        bounding_edge_lst: List[NetEdgeInput] = []
+
+        # add nodes if new
+        edge_nodes: Tuple[int,int] = new_edge[0]
+        # check both nodes in edge
+        for v in edge_nodes:
+            # only care if nodes are not already in graph
+            if v not in minimal.nodes:
+                # track nodes and bound them by min and max times
+                node_add_lst.append(v)
+                v_bounding_edges = get_bounding_edges(v)
+                bounding_edge_lst += v_bounding_edges
+
+    # performs path consistency algorithm on minimal STN after adding single edge
+    # returns tuple (success_flag, minimal_edge_add_lst, minimal_edge_remove_lst)
+    # success is achieved if no inconsistent edge triple is found
+    # minimal_edge_add_lst: all edges that were added before return
+    # minimal_edge_remove_lst: all edges that were removed before return
+    def path_consistency( self, min_stn: DiGraph, new_edge: NetEdgeInput
+                          ) -> Tuple[bool, List[NetEdgeInput], List[NetEdgeInput]]:
+        intersect_edges = self.intersect_edges
+        find_edge = self.find_edge
+        minimal_edge_add_lst: List[NetEdgeInput] = []
+        minimal_edge_remove_lst: List[NetEdgeInput] = []
+        # if there is an existing edge parallel to new_edge terminate early with success
+        # if new_edge is no stricter than the existing edge
+        # check for edge in minimal STN, may need to get reverse edge and flip it
+        existing_edge = find_edge(min_stn,*new_edge[0])
+        intersected_edge = new_edge
+        if existing_edge is not None:
+            intersected_edge = intersect_edges(new_edge,existing_edge)
+            if intersected_edge == existing_edge:
+                return (True,minimal_edge_add_lst,minimal_edge_remove_lst)
+            # remove existing edge
+
+        # add intersected_edge
+
         pass
+
+    # return edge if one exists between node_0 and node_1
+    # if edge (node_0, node_1) does not exist will return edge (node_1, node_0)
+    # in the later case will return flipped edge
+    # if none of these are true will return None
+    # CURRENTLY WRONG
+    # NEED TO UPDATE TO PROPERLY FLIP AND INDICATE IT
+    def find_edge( self, graph, node_0: int, node_1: int ) -> NetEdgeInput:
+        edge = (node_0,node_1)
+        if (node_0,node_1) in graph.edges:
+            edge_dict = graph.get_edge_data(node_0,node_1)
+        elif (node_1,node_0) in graph.edges:
+            edge_dict = graph.get_edge_data( node_1, node_0 )
+        else:
+            return None
+        return (edge, edge_dict)
+
+    # given edge (i,j) and edge (j,k) gives composed edge (i,k)
+    # adds minimum and maximum delta t's
+    def compose_edges( self, edge_ij: NetEdgeInput, edge_jk: NetEdgeInput ) -> NetEdgeInput:
+        edge_ik = ( edge_ij[0][0], edge_jk[0][1] )
+        edge_ik_dict = {
+            "min_delta_t": edge_ij[1]["min_delta_t"] + edge_jk[1]["min_delta_t"],
+            "max_delta_t": edge_ij[ 1 ][ "max_delta_t" ] + edge_jk[ 1 ][ "max_delta_t" ],
+        }
+        return (edge_ik,edge_ik_dict)
+
+    # given 2 parallel edges get intersection
+    # edge_ij intersect edge_ij' has max min_delta_t and the min max_delta_t
+    def intersect_edges( self, edge: NetEdgeInput, edge_prime: NetEdgeInput ) -> NetEdgeInput:
+        if edge[0] != edge[1]:
+            raise "cannot intersect two edges that are not parallel"
+        edge_ik_dict = {
+            "min_delta_t": max( edge[1]["min_delta_t"], edge_prime[1]["min_delta_t"] ),
+            "max_delta_t": min( edge[ 1 ][ "max_delta_t" ], edge_prime[ 1 ][ "max_delta_t" ] ),
+        }
+        return (edge[0],edge_ik_dict)
+
+    # edge_ij, edge_jk, and edge_ik consistency check
+    # returns  intersect( compose( edge_ij, edge_jk ), edge_ik ) if result is not empty else None
+    # the intersection is empty if the resulting edge has max_delta_t < min_delta_t
+    def consistency_check( self, edge_ij: NetEdgeInput, edge_jk: NetEdgeInput, edge_ik: NetEdgeInput
+                           ) -> Union[None,NetEdgeInput]:
+        composed_edge = self.compose_edges(edge_ij,edge_jk)
+        intersected_edge = self.intersect_edges(composed_edge,edge_ik)
+        if intersected_edge[1]["max_delta_t"] < intersected_edge[1]["min_delta_t"]:
+            return intersected_edge
+        else:
+            return None
+
+    # returns minimal and maximal bound edges for a node as 2 entry list
+    # 0: minimal edge, 1: maximal edge
+    def get_bounding_edges( self, node: int ) -> List[NetEdgeInput,NetEdgeInput]:
+        t_min = self.t_min
+        t_max = self.t_max
+        min_bound_edge = self.get_formatted_edge( (-1, "<=", v, t_min) )
+        max_bound_edge = self.get_formatted_edge( (-2, ">=", v, t_max) )
+        return [min_bound_edge, max_bound_edge]
 
     # add temporal constraints to full and minimal networks
     # if inconsistency found, restore networks to before call
@@ -139,7 +242,8 @@ class TemporalNetwork:
                 val -= 1
             elif t_op is ">":
                 val += 1
-            raise ValueError( str( t_op ) + " is not a valid time point comparison operator" )
+            else:
+                raise ValueError( str( t_op ) + " is not a valid time point comparison operator" )
             t_op = t_op + "="
         # transform in to edge tuple for networkx
         t_min = self.t_min
@@ -148,11 +252,11 @@ class TemporalNetwork:
         edge = ( tp_0, tp_1 )
         edge_dict = dict()
         if t_op == "<=":
-            edge_dict.update( { min_delta_t: val, max_delta_t: t_range } )
+            edge_dict.update( { "min_delta_t": val, "max_delta_t": t_range } )
         elif t_op == "==":
-            edge_dict.update( { min_delta_t: val, max_delta_t: val } )
+            edge_dict.update( { "min_delta_t": val, "max_delta_t": val } )
         elif t_op == ">=":
-            edge_dict.update( { min_delta_t: -t_range, max_delta_t: -val } )
+            edge_dict.update( { "min_delta_t": -t_range, "max_delta_t": -val } )
         return ( edge, edge_dict )
 
 
