c8147c8da95d405ffe0b315f263742d5544e959a
pzaidins2 <paulzaidins2@gmail.com>
2026-04-21 15:04:18 -0400

    reduced actions to single time points

--- examples/blocks_world/temporal/blocks_world_temporal_actions.py
+++ examples/blocks_world/temporal/blocks_world_temporal_actions.py
@@ -11,8 +11,9 @@ Surface = NewType( "Surface", str )
 Block = NewType( "Block", Surface )
 Table = NewType( "Table", Surface )
 
-type ObjectVarAssertion = Tuple[ int, *Tuple[ Any, ... ], bool ]
-type ObjectVarPersistence = Tuple[ int, int, *Tuple[ Any, ... ], bool ]
+type ObjectVarAssertion = Tuple[ int, str, *Tuple[ Any, ... ], bool ]
+type GenericObjectVarAssertion = Tuple[ str, *Tuple[ Any, ... ], bool ]
+type ObjectVarPersistence = Tuple[ int, int, str, *Tuple[ Any, ... ], bool ]
 
 
 class TemporalBlocksWorldDomainObjects( Protocol ):
@@ -76,6 +77,7 @@ def safe_list_update(lst: List, update_element_lst: List, update_start_index: in
     # new last valid index
     last_valid_index = update_start_index + update_size - 1
     # if list exists at index replace else extend
+    current_index = -1
     for i in range( update_size ):
         current_index = update_start_index + i
         # out of bounds add remainder via extend
@@ -89,113 +91,31 @@ def safe_list_update(lst: List, update_element_lst: List, update_start_index: in
     return last_valid_index
 
 
-# returns generator that yields potentially relevant variable assignments
-# given a list of tuples, the last valid index, and a predicate and list of predicate values return
-# the first match going in reverse order of indices
-# given (t_0, on, A, B), (t_0, on, B, C), (t_1, on, A, T), (t_1, clear, A)
-# the input (on,) would give (t_1, on, A, T) and input (on,B) would
-# give (t_0,on,B,C)
-# if no pattern match returns empty generator else returns generator for matching pattern
-def object_variable_lookup_generator(object_var_lst: List[ Tuple ], last_valid_index: int, pred_pat: Tuple,
-                                     t_ordered: List[ int ],
-                                     t_now: int):
-    # cycle through list in reverse
-    for i in reversed( range( last_valid_index + 1 ) ):
-        current_tuple = object_var_lst[ i ]
-        for j in range( len( pred_pat ) ):
-            # break out of inner loop on mismatch
-            if current_tuple[ j + 1 ] != pred_pat[ j ]:
-                break
-            # match found if every arg in pred_pat matched
-            if j == len( pred_pat ) - 1:
-                # make sure the time point is before t_now in the ordering
-                t_then = current_tuple[ 0 ]
-                now_index = t_ordered.index( t_now )
-                then_index = t_ordered.index( t_then )
-                if now_index >= then_index:
-                    match = current_tuple
-                    yield match
-
-
-# returns generator which yields potentially relevant temporal constraints
-# the following are considered relevant
-def temporal_constraint_lookup_generator(temporal_con_lst: List[ Tuple ], last_valid_index: int, temporal_pat: Tuple,
-                                         t_ordered: List[ int ], t_now: int):
-    # # cycle through list in reverse starting at last valid index
-    # for temporal_con in reversed(temporal_con_lst[:last_valid_index+1]):
-    #     t_con_0, t_op, t_con_1 = temporal_con
-    pass
-
-
-# check for evaluated predicate contradicting any member of the persistent conditions
-# a contradiction requires the current time point to be within the bound of a persistence condition starting and
-# ending times with the predicate and predicate arguements matching and the values being different
-# only persistence conditions at indices smaller than last valid index count
-def persistence_check(pers_con_lst: List[ Tuple ], pers_con_last_val_idx: int,
-                      eval_pred: Tuple, t_ordered: List[ int ], t_now: int):
-    # iterate in reverse over persistences within last valid index
-    for pers_con in reversed( pers_con_lst[ :pers_con_last_val_idx + 1 ] ):
-        # check if t_now is within persistence condition bound
-        t_start = pers_con[ 0 ]
-        t_end = pers_con[ 1 ]
-        t_now_idx = t_ordered.index( t_now )
-        t_start_idx = t_ordered.index( t_start )
-        t_end_idx = t_ordered.index( t_end )
-        if t_now_idx >= t_start_idx and t_now_idx <= t_end_idx:
-            # check if predicate name and args match
-            if eval_pred[ 1:-1 ] == pers_con[ 2:-1 ]:
-                # check if value contradicts
-                if eval_pred[ -1 ] != pers_con[ -1 ]:
-                    return False
-    return True
-
-
-# return true if no contradiction in state would be created by adding new tuple
-# object variable 
-def contradiction_check(lst_type: str, lst: List[ Tuple ], eval_pred: Tuple, t_ordered: List[ int ], t_now: int):
-    if lst_type == "object":
-        return object_contradiction_check()
-    elif lst_type == "temporal":
-        return temporal_contradiction_check()
-    else:
-        raise ValueError( lst_type + "is not a supported value for the type of list" )
-
-
-# if an assignment at the time of the predicate exists already, returns False if the value is different else
-# returns True
-def object_contradiction_check(object_var_lst: List[ Tuple ], last_valid_index: int, pred_pat: Tuple,
-                               t_ordered: List[ int ], t_now: int):
-    # find any assignment at t_now for the predicate
-    rel_obj_gen = object_variable_lookup_generator( object_var_lst, last_valid_index, pred_pat[ :-1 ], t_ordered,
-                                                    t_now )
-    for rel_obj_var in rel_obj_gen:
-        # check if value is different
-        if rel_obj_var[ -1 ] != pred_pat[ -1 ]:
+# given state specified by state_references and state_values find whether object_var_assertion
+# is true
+# this happens if object_var[1:] is found at a time point before object_var[0] and the negation is not
+def check_object_assertion(
+        state_references: State, state_values: TemporalBlocksWorldStateValues,
+        object_var_assertion: ObjectVarAssertion,
+):
+    # object variable assertion without time
+    generic_assertion: GenericObjectVarAssertion = (object_var_assertion[ 1 ], *object_var_assertion[ 2: ])
+    # object variable assertion negated
+    negated_generic_assertion: GenericObjectVarAssertion = (*generic_assertion, not (generic_assertion[ -1 ]))
+    # predicate to search
+    predicate_label: str = generic_assertion[ 0 ]
+    # list to be searched (cutoff based on reference index
+    # assertions are placed in order of t_ordered
+    search_lst: List[ ObjectVarAssertion ] = \
+        state_values.__getattribute__( predicate_label )[ :state_references.__getattribute__( predicate_label ) ]
+    # iterate over list in reverse
+    for obj_var_assert in reversed( search_lst ):
+        generic_this: GenericObjectVarAssertion = (obj_var_assert[ 1 ], *obj_var_assert[ 2: ])
+        if generic_this == negated_generic_assertion:
             return False
-    return True
-
-
-def temporal_contradiction_check():
-    pass
-
-
-# return true if tuple can be added without contradiction or persistence condition violation
-def can_add_assertion():
-    if contradiction_check() and persistence_check():
-        return True
-
-
-# given list of assertions check to see if they can all be added without issue
-def can_add_all_assertions():
-
-    pass
-
-
-# get all time points that are plausible candidates for next time point to add to ordering
-# find all timepoints that have no timepoint not in t_ordered that must precede them
-def next_timepoint_generator():
-    pass
-
+        elif generic_this == generic_assertion:
+            return True
+    return False
 
 # ADD universal low priority methods for advancing time and choosing next timepoint
 # NEED actions will need wrapper methods to work for goals
@@ -204,34 +124,26 @@ actions = Actions()
 
 
 # from t_start to t_end=t_start+1 move block from being on start_pos to being on the table
-def move_block_to_table(state_references: State, temporal_network: TemporalNetwork,
-                        state_values: TemporalBlocksWorldStateValues,
-                        temporal_goal: ObjectVarAssertion,
-                        t_start: int, block: Block, start_pos: Block, table: Table):
-    # retrieve goal values and some checks
-    t_end: int = temporal_goal[ 0 ]
-    predicate_label, top_block, bot_block = temporal_goal[ 1:-1 ]
-    bool_val: bool = temporal_goal[ -1 ]
-    # goal is relevant goal and parameters match goal
-    if all( [
-        predicate_label == "on",
-        type( block ) == Block,
-        type( table ) == Table,
-        top_block == block,
-        bot_block == start_pos,
-        bool_val,
-    ] ):
-        # temporal network consistency check
-        # t_end == t_start + 1
-        temporal_constraint = (t_end, "==", t_start, 1)
-        success_flag, node_add_lst, edge_add_lst, edge_remove_lst = temporal_network.add_temporal_constraints_from(
-                [
-                    temporal_constraint,
-                ] )
-        if success_flag:
-            object_var_assertions: List[ ObjectVarAssertion ] = [
-                (t_end, "on", top_block, table, True),
+def tga_move_block_to_table_start(
+        state_references: State, temporal_network: TemporalNetwork, state_values: TemporalBlocksWorldStateValues,
+        t_start: int, block: Block, start_pos: Block,
+):
+    object_var_assertion: ObjectVarAssertion = (t_start, "on", block, start_pos, True)
+    # check assertion and constraints
+    if all(
+            [
+                type( block ) == Block,
+                type( start_pos ) == Block,
+                check_object_assertion( state_references, state_values, object_var_assertion ),
             ]
+    ):
+        # this action is t_start and only checks constraints, return
+        return state_references
+
+
+# conditions at t_start are met
+# [t_start] on(block) = start_pos, start_pos is a block
+# check that on(block) = start_pos
 
 
 # Need temporal extension of actions
--- examples/blocks_world/temporal/test_blocks_world_temporal.py
+++ examples/blocks_world/temporal/test_blocks_world_temporal.py
@@ -1,5 +1,4 @@
 # content of test_approx.py
-import pytest
 
 from examples.blocks_world.temporal.blocks_world_temporal_actions import safe_list_update
 
@@ -44,7 +43,7 @@ def test_safe_list_update_4():
     assert lst == [0, 1, 2, 3, 4, 6, 7, 8]
 
 
-def test_safe_list_update_1():
+def test_safe_list_update_5():
     lst = [0, 1, 2, 3, 4, 5]
     update_index = 1
     update_element_lst= [6, 7, 8]
