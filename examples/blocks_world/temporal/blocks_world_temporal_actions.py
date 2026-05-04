a00243b02cb4e90f356e48610c92dc26ae4b98f5
pzaidins2 <paulzaidins2@gmail.com>
2026-05-04 18:46:37 -0400

    outline of temporal planner algorithm added

--- examples/blocks_world/temporal/blocks_world_temporal_actions.py
+++ examples/blocks_world/temporal/blocks_world_temporal_actions.py
@@ -64,7 +64,30 @@ Note: rollbacks of values in State object are automatically handled by IPyHOP bu
 mechanisms
 must be added to roll back everything else.
 '''
+'''
+should attempt the following in order
+1) resolve goal -> 
+pre: action achieves goal at t_now
+effect: removes goal and add persistence condition from t_now to t_goal
+2) decompose goal -> 
+pre: method for goal exists and would not add persistence condition contradiction
+effect: removes goal, add new goals (with preexisting or new timepoints), add method persistence conditions
+3) place new timepoint in ordering -> 
+pre: time point that could be after last time point in ordering exists and no time
+point not in ordering must be before the candidate time point, no goal for last ordered time point can be open
+effect: candidate time point appended to ordering, t_last == t_candidate added as temporal constraint, 
+adds t_candidate <= t_i for t_i in t_unordered
+4) advance t_now ->
+pre: time point that could be after last time point in ordering exists and no time
+point not in ordering must be before the candidate time point, no goal for last ordered time point can be open, and
+effect: candidate time point appended to ordering, t_last < t_candidate added as temporal constraint,
+t_now set equal to candidate time point
 
+each of these choices is a decision point
+if deadend is reached, backtrack to previous decision point
+if deadend is reached and not previous decision point exists there is no solution
+
+'''
 
 # helper functions
 # bulk insert elements into list overwriting and appending as needed
@@ -94,7 +117,8 @@ def safe_list_update(lst: List, update_element_lst: List, update_start_index: in
 # given state specified by state_references and state_values find whether object_var_assertion
 # is true
 # this happens if object_var[1:] is found at a time point before object_var[0] and the negation is not
-def check_object_assertion(
+# ASSUMPTION: assertions inserted in same order as t_ordered
+def verify_object_assertion(
         state_references: State, state_values: TemporalBlocksWorldStateValues,
         object_var_assertion: ObjectVarAssertion,
 ):
@@ -107,7 +131,7 @@ def check_object_assertion(
     # list to be searched (cutoff based on reference index
     # assertions are placed in order of t_ordered
     search_lst: List[ ObjectVarAssertion ] = \
-        state_values.__getattribute__( predicate_label )[ :state_references.__getattribute__( predicate_label ) ]
+        state_values.__getattribute__( predicate_label )[ :(state_references.__getattribute__( predicate_label )) ]
     # iterate over list in reverse
     for obj_var_assert in reversed( search_lst ):
         generic_this: GenericObjectVarAssertion = (obj_var_assert[ 1 ], *obj_var_assert[ 2: ])
@@ -117,37 +141,75 @@ def check_object_assertion(
             return True
     return False
 
-# ADD universal low priority methods for advancing time and choosing next timepoint
+
+# add change assertions to state values of object_var and update state reference indices
+def add_object_var_changes():
+    # safe_list_add()
+    pass
+
+
+# add persistence assertions to state values of persistences and update persistence reference indices
+def add_object_var_persistences():
+    # safe_list_add()
+    pass
+
+
+# returns true if the negation of the given object var assertion does not exist else False
+# example:
+# let the new change assertion be [t_now] foo=bar
+# [t_now] foo=not(bar) cannot exist in the change assertions
+def check_change_existing_changes_safe(
+        state_references: State, state_values: TemporalBlocksWorldStateValues,
+        object_var_assertion: ObjectVarAssertion,
+) -> bool:
+    pass
+
+
+def check_change_persistences_safe():
+    pass
+
+
+def check_persistence_assertions_safe():
+    pass
+
+
+def check_persistence_existing_persistences_safe():
+    pass
+
+
+# NEED universal low priority methods for advancing time and choosing next timepoint
 # NEED actions will need wrapper methods to work for goals
+# NEED pseudoactions that are essentially methods that can't be split during repair
 
 actions = Actions()
 
 
 # from t_start to t_end=t_start+1 move block from being on start_pos to being on the table
+# corresponds to pseudoaction move_block_to_table
+# tga_move_block_to_table_start checks conditions at t_start
+# tga_move_block_to_table_end check
 def tga_move_block_to_table_start(
         state_references: State, temporal_network: TemporalNetwork, state_values: TemporalBlocksWorldStateValues,
         t_start: int, block: Block, start_pos: Block,
 ):
     object_var_assertion: ObjectVarAssertion = (t_start, "on", block, start_pos, True)
     # check assertion and constraints
+    # conditions at t_start are met
+    # [t_start] on(block) = start_pos, start_pos is a block
+    # check that on(block) = start_pos
     if all(
             [
                 type( block ) == Block,
                 type( start_pos ) == Block,
-                check_object_assertion( state_references, state_values, object_var_assertion ),
+                verify_object_assertion( state_references, state_values, object_var_assertion ),
             ]
     ):
         # this action is t_start and only checks constraints, return
         return state_references
 
 
-# conditions at t_start are met
-# [t_start] on(block) = start_pos, start_pos is a block
-# check that on(block) = start_pos
-
-
-# Need temporal extension of actions
-actions.declare_actions( [ move_block_to_table, ] )
+# NEED temporal extension of actions
+actions.declare_actions( [ tga_move_block_to_table_start, ] )
 
 # ******************************************    Demo / Test Routine         ****************************************** #
 if __name__ == '__main__':
